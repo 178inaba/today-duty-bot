6aa48f8032594d09062ddd3dd8a1b0ef54bc656c
Masahiro Furudate <178inaba.git@gmail.com>
2024-05-26 03:31:24 +0900

    Add sql

--- go.mod
+++ go.mod
@@ -4,7 +4,12 @@ go 1.22.3
 
 require (
 	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/jmoiron/sqlx v1.4.0
 	github.com/slack-go/slack v0.13.0
 )
 
-require github.com/gorilla/websocket v1.4.2 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -9,11 +11,29 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
 )
 
 func main() {
+	ctx := context.Background()
+
+	db, err := openSqlxDB(
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_PROTOCOL"),
+		os.Getenv("MYSQL_ADDRESS"),
+		os.Getenv("MYSQL_DB_NAME"),
+	)
+	if err != nil {
+		log.Fatalf("Open database: %v.", err)
+	}
+	if err := db.PingContext(ctx); err != nil {
+		log.Fatalf("Ping database: %v.", err)
+	}
+
 	api := slack.New(os.Getenv("SLACK_TOKEN"))
 	signingSecret := os.Getenv("SLACK_SIGNING_SECRET")
 
@@ -33,6 +53,24 @@ func main() {
 	if err := http.ListenAndServe(":"+port, r); err != nil {
 		log.Fatalf("End listen and serve: %v.", err)
 	}
+}
+
+func openSqlxDB(user, passwd, net, addr, dbName string) (*sqlx.DB, error) {
+	c := mysql.NewConfig()
+	c.User = user
+	c.Passwd = passwd
+	c.Net = net
+	c.Addr = addr
+	c.DBName = dbName
+	c.Collation = "utf8mb4_bin"
+	c.ParseTime = true
+
+	db, err := sqlx.Open("mysql", c.FormatDSN())
+	if err != nil {
+		return nil, fmt.Errorf("open sqlx: %w", err)
+	}
+
+	return db, nil
 }
 
 type Handler struct {
